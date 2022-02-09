6ec066f28d416f160c84edf8e778c65c6adb2208
Anthony JACQUEMIN <anthonyjacquemin@hotmail.fr>
2022-02-09 04:38:20 +0100

    Tic tac toe step 4

--- tictactoe/tictactoe.kt
+++ tictactoe/tictactoe.kt
@@ -3,6 +3,14 @@ package tictactoe
 import java.util.Scanner
 import kotlin.math.abs
 
+fun showGrid(grid: CharArray) {
+    println("-".repeat(9))
+    println("| ${grid[0]} ${grid[1]} ${grid[2]} |")
+    println("| ${grid[3]} ${grid[4]} ${grid[5]} |")
+    println("| ${grid[6]} ${grid[7]} ${grid[8]} |")
+    println("-".repeat(9))
+}
+
 fun isWinner(grid: CharArray, player: Char): Boolean {
     var win = false
     for (i in 0..2) {
@@ -21,11 +29,8 @@ fun main() {
     print("Enter cells: ")
     val scan = Scanner(System.`in`)
     val grid = scan.nextLine().toCharArray()
-    println("-".repeat(9))
-    println("| ${grid[0]} ${grid[1]} ${grid[2]} |")
-    println("| ${grid[3]} ${grid[4]} ${grid[5]} |")
-    println("| ${grid[6]} ${grid[7]} ${grid[8]} |")
-    println("-".repeat(9))
+    showGrid(grid)
+    /*
     if (abs(grid.count {c -> c == 'X'} - grid.count {c -> c == 'O'}) > 1) {
         println("Impossible")
     } else {
@@ -44,6 +49,27 @@ fun main() {
         } else {
             println("Draw")
         }
+    }*/
+    var validInput = false
+    var cellNumber = 0
+    while (!validInput) {
+        print("Enter the coordinates: ")
+        var coord = readLine()!!.split(" ")
+        var posX = coord[1].toIntOrNull()
+        var posY = coord[0].toIntOrNull()
+        if (posX == null || posY == null)
+            println("You should enter numbers!")
+        else if (posX > 3 || posX < 1 || posY > 3 || posY < 1)
+            println("Coordinates should be from 1 to 3!")
+        else {
+            cellNumber = (posY - 1) * 3 + (posX - 1)
+            if (grid[cellNumber] != '_')
+                println("This cell is occupied! Choose another one!")
+            else
+                validInput = true
+        }
+        
     }
-    
+    grid[cellNumber] = 'X'
+    showGrid(grid)
 }
