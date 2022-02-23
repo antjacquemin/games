c545f07ed10679322e8369153a4180b2b588b385
Anthony JACQUEMIN <anthonyjacquemin@hotmail.fr>
2022-02-23 10:41:37 +0100

    Minesweeper Step 3

--- minesweeper/minesweeper.kt
+++ minesweeper/minesweeper.kt
@@ -4,12 +4,33 @@ import kotlin.random.Random
 
 fun showGrid(grid: BooleanArray) {
     var cell = 0
+    val neigborhoodFull = intArrayOf(-10, -9, -8, -1, 1, 8, 9, 10)
+    val neigborhoodLeft = intArrayOf(-10, -9, -1, 8, 9)
+    val neigborhoodRight = intArrayOf(-9, -8, 1, 9, 10)
+    var neigborhood = neigborhoodFull
+    var index = 0
+    var nbMines = 0
     repeat(9) {
         repeat(9) {
             if (grid[cell])
                 print("X")
-            else
-                print(".")
+            else {
+                nbMines = 0
+                when {
+                    cell % 9 == 0 -> neigborhood = neigborhoodRight
+                    cell % 9 == 8 -> neigborhood = neigborhoodLeft
+                    else -> neigborhood = neigborhoodFull 
+                }
+                for (i in neigborhood) {
+                    index = cell + i
+                    if (index > -1 && index < 81 && grid[index])
+                        nbMines++
+                }
+                if (nbMines == 0)
+                    print(".")
+                else
+                    print(nbMines)
+            }
             cell++
         }
         println()
